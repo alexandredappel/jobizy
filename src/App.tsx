286390f8c0b135e7eb6fb7e612c0dc1071cb8998
alexandredappel <alexandre@sitewebartisan.fr>
2025-01-13 17:56:58 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,7 @@ const App = () => (
               } />
               
               {/* Worker Routes */}
-              <Route path="/worker/onboarding" element={
+              <Route path="/pages/worker/onboarding" element={
                 <ProtectedRoute role="worker">
                   <WorkerOnboarding />
                 </ProtectedRoute>
